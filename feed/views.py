1ef2d6aee19efc722fc2046debbb649deba2f479
Sahai-A <s-sahaia@bsd405.org>
2018-02-20 17:47:34 -0800

    Set up to render templates

--- feed/views.py
+++ feed/views.py
@@ -1,11 +1,17 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 from .models import User
-
+from django.shortcuts import render
 
 def index(request):
 	return HttpResponse("Hello, world. You're at the index")
 	
 def profile(request):
-	profile_info = User.objects.get(pk=1)
-	text_output = profile_info.name + '     ' + profile_info.email
-	return HttpResponse(text_output)
+	try:
+		user = User.objects.get(pk=1)
+	except User.DoesNotExist:
+		raise Http404("User does not exist")
+	return render(request, 'feed/index.html', {'user': user})
+	
+	
+	#text_output = profile_info.name + '     ' + profile_info.email
+	#return HttpResponse(text_output)
