84016bc9d7963ed1c1389c59c71ca1a4f135dbc3
Sahai-A <s-sahaia@bsd405.org>
2018-03-06 17:05:51 -0800

    linked viewing the user profile from users feed

--- feed/urls.py
+++ feed/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
 	path('<int:user_id>', views.index, name = 'index'),	
-	path( 'profile/<int:user_id>', views.profile, name='profile'),
+	path('profile/<int:user_id>', views.profile, name='profile'),
+	path('profile/new', views.new_profile, name='new_profile'),
+	path('<int:user_id>/submit-profile', views.submit_profile, name='submit_profile')
 ]
--- feed/views.py
+++ feed/views.py
@@ -1,5 +1,6 @@
-from django.http import HttpResponse, Http404
+from django.http import HttpResponse, Http404, HttpResponseRedirect
 from .models import User
+from django.urls import reverse
 from django.shortcuts import get_object_or_404, render
 
 def index(request, user_id):
@@ -9,6 +10,13 @@ def index(request, user_id):
 def profile(request, user_id):
 	user = get_object_or_404(User, pk=user_id)
 	return render(request, 'feed/profile.html', {'user': user})
+	
+def new_profile(request):
+	return render(request, 'feed/new_profile.html',)
+	
+def submit_profile(request, user_id):
+	return render(request,'feed/submit_profile,html',)
+	
 	#try:
 	#	user = User.objects.get(pk=user_id)
 	#except User.DoesNotExist:
