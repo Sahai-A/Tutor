7426c11fb89d6bb2d1b47e82b954ff7803efbaa9
Sahai-A <s-sahaia@bsd405.org>
2018-04-03 18:44:59 -0700

    added phone number column to database

--- feed/models.py
+++ feed/models.py
@@ -2,7 +2,8 @@ from django.db import models
 
 class User(models.Model):
 	name = models.CharField(max_length=20)
-	email = models.CharField(max_length=40, default = "some string")
+	email = models.CharField(max_length=40, default = "some email address")
+	phone_number = models.CharField(max_length=11, default = "")
 	def __str__(self):
 		return self.name
 # Create your models here.
